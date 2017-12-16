5a293296601b0eff453349d64ae56b32016e0ef8
Ravindra <ravindra@hellskippers.com>
2017-12-16 15:34:57 +0530

    First Commit

---
+++ SampleHellSkippersLoginPage/SampleHellSkippersLoginPage/Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿using SampleHellSkippersLoginPage.DbConnect;
+using SampleHellSkippersLoginPage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleHellSkippersLoginPage.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult LoginPage()
+        {
+            ViewBag.ErrorMsg = TempData["ErrorMessage"];
+            return View();
+        }
+        public ActionResult DashboardPage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CheckLogin([Bind(Include = "Username")]User user)
+        {
+            return RedirectToAction("DashboardPage");
+        }
+        //[HttpPost]
+        //public ActionResult CheckLogin(FormCollection fc)
+        //{
+        //    String un = fc["Username"].ToString();
+        //    String pa = fc["Password"].ToString();
+        //    return RedirectToAction("DashboardPage");
+        //}
+        //[HttpPost]
+        //public ActionResult CheckLogin(String Username,String Password)
+        //{
+        //    DbFile db = new DbFile();
+        //    var userlist =db.user.Where(x => x.Username == Username && x.Password == Password);
+        //    bool isValid = userlist.ToList().Count>0;
+        //    if (isValid)
+        //    {
+        //        return RedirectToAction("DashboardPage");
+        //    }
+        //    else
+        //    {
+        //        TempData["ErrorMessage"] = "Invalid Credantials";
+        //        return RedirectToAction("LoginPage");
+        //    }
+        //}
+    }
+}
---
+++ SampleHellSkippersLoginPage/SampleHellSkippersLoginPage/DbConnect/DbFile.cs
@@ -0,0 +1,14 @@
+﻿using SampleHellSkippersLoginPage.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SampleHellSkippersLoginPage.DbConnect
+{
+    public class DbFile :  DbContext
+    {
+        public DbSet<User> user { get; set; }
+    }
+}
